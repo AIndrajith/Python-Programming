99fcca78fd9431a6ff87d02e81008ad10ca8b562
AIndrajith <deegalla12378@usci.ruh.ac.lk>
2025-01-11 20:54:16 +0530

    created programmes for while loop and converting puedo code to python code

---
+++ src/Chapter 3/maitre_d.py
@@ -0,0 +1,16 @@
+#Program No 8
+
+#Maitre D'
+#Demonstrates treating a value as a condition
+
+print("Welcome to the Chateau D' Food")
+print("It seems we are quite full this evening.\n")
+
+money = int(input("How many dollars do you slip the Maitre D'? "))
+
+if money:
+    print("Ah, I am reminded of a table. Right this way")
+else:
+    print("please, sit. It may be a while.")
+    
+input("\n\nPress the enter key to exit.")    
