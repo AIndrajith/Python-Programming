fff3aa3e0a42b93a597097e29fa96d118099fbb3
AIndrajith <deegalla12378@usci.ruh.ac.lk>
2025-01-11 16:52:35 +0530

    created codes for demonstrating the random number generation, if , if else and elif

---
+++ src/Chapter 3/granted_or_denied.py
@@ -0,0 +1,16 @@
+#Program No 3
+
+#Granted or Denied
+#Demonstrates an else clause
+
+print("Welcome to System Security Inc.")
+print("--where security is our middle name\n")
+
+password = input("Enter your Password: ")
+
+if password == "secret":
+    print("Access Granted.")
+else:
+    print("Access Denied")
+
+input("\n\nPress the enter key to exit.")    
---
+++ src/Chapter 3/password.py
@@ -0,0 +1,14 @@
+#Program No 2
+
+#Password
+#Demonatrates the If statement
+
+print("Welcome to System Security Inc.")
+print("--wher security is our middle name\n")
+
+password = input("Enter your Password: ")
+
+if password == "secret":
+    print("Access Granted")
+
+input("\n\nPress the enter key to exit.")
