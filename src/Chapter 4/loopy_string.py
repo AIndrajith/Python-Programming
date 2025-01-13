bc66a540159bfd14447b6b3f4d094b271fdf245a
AIndrajith <deegalla12378@usci.ruh.ac.lk>
2025-01-13 22:15:15 +0530

    created programmes for demonstrating for loop and range function

---
+++ src/Chapter 4/loopy_string.py
@@ -0,0 +1,12 @@
+#Program No 1
+
+#Loopy String 
+#Demonstrates the for Loop with a string
+
+word = input("Enter a word: ")
+
+print("\nHere's each letter in your word:")
+for letter in word:
+    print(letter)
+
+input("\n\nPress the enter key to exit.")    
