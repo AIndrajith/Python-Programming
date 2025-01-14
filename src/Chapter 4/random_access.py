aec06553fe6fa195c3e868a01a927de3040155d3
AIndrajith <deegalla12378@usci.ruh.ac.lk>
2025-01-15 00:00:08 +0530

    programmes for demonstrating string indexing

---
+++ src/Chapter 4/random_access.py
@@ -0,0 +1,18 @@
+#Program No 4
+
+#Random Access
+#Demonstrates string Indexing
+
+import random
+
+word = "index"
+print("The word is: ",word,"\n")
+
+high = len(word)
+low = -len(word)
+
+for i in range(10):
+    position = random.randrange(low,high)
+    print("word [", position, "]\t", word[position])
+
+input("\n\nPress the enter key to exit.")    
